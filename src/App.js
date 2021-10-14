d0ca096f3fe48575383bd4368055fc2ac82c5e90
Travis Fox <travis.fox90@gmail.com>
2021-10-13 23:25:47 -0400

    setup routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import NavBar from './components/navBar';
+import Home from './components/Home'
+import SkuContainer from './containers/SkuContainer'
+import OrderContainer from './containers/OrderContainer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <div>
+      <NavBar />
+
+      <Route
+        exact path="/" render={(routerProps)=>
+          <Home routerProps={routerProps}/> }
+      />
+
+      <Route
+        path="/skus" 
+        render={routerProps => <SkuContainer {...routerProps}/>}
+      />
+
+      <Route
+        path="/orders" 
+        render={routerProps => <OrderContainer {...routerProps}/>}
+      />
+        
+      </div>
     </div>
   );
 }
