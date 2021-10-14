9a6966bfac2347bd1e0a15e82a475e3d6819fe92
Travis Fox <travis.fox90@gmail.com>
2021-10-13 22:50:28 -0400

    created Sku container it renders sku

---
+++ src/containers/SkuContainer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Sku from '../components/Sku.js'
+
+
+
+class SkuContainer extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <Sku />
+            </div>
+        )
+    }
+
+}
